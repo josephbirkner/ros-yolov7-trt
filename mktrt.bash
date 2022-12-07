e68241b2656ac04c21f3a8a9e3d7f7f338d619db
realitivity <you@example.com>
2022-12-07 13:05:44 +0100

    Fix TRT export script

--- mktrt.bash
+++ mktrt.bash
@@ -6,35 +6,35 @@ set -euo pipefail
 # Create Yolo v7 mask models for 640 and 1280 resolutions.
 #
 
-# Export to ONNX
 . /venv/bin/activate
 
 resolutions=( 640 1280 )
 
 for resolution in "${resolutions[@]}"; do
     model_name="yolov7_$resolution"
-    onnx_file="$model_name.onnx"
 
     echo "Running ONNX export for $model_name ..."
     cd /yolov7
     mkdir -p onnx
+    mkdir -p /trt
     python export.py \
-        --include onnx \
+        --include onnx engine\
+        --data data/coco.yaml \
         --weights ./yolov7-seg.pt \
-        --topk-all 100 \
+        --topk-per-class 100 \
+        --topk-all 1000 \
+        --device 0 \
+        --half \
         --iou-thres 0.65 \
         --conf-thres 0.35 \
-        --imgsz "$resolution" "$resolution" \
-        --simplify
+        --imgsz "$resolution" \
+        --simplify \
+        --verbose
 
-    mv yolov7-seg.onnx "$onnx_file"
-
-    # Export to TRT
-    echo "Running ONNX export for $onnx_file ..."
-    cd yolov7-trt-helper
-    mkdir -p /trt
-    python export.py -o "/yolov7/$onnx_file" -e "/trt/$model_name.trt" -p fp16
+    mv yolov7-seg.engine /trt/"$model_name".trt
 done
 
+chmod a+rw /trt
 chmod a+rw /trt/*
+
 echo "Done."
